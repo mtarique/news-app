2f80ff18d66c1985d707b6da97713991fd1447ec
mtarique <mtarique@outlook.com>
2024-04-09 00:55:02 +0530

    Bug fixed where entertainment category updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,8 @@ export class Navbar extends Component {
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
-                                </li>
-                                <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                                <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
+                                <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                                 <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
                                 <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
